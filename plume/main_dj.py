b57bffe14dd852120b51fa16b3bd6cfbfbd19453
lolrenceH <lawrenhu@bu.edu>
2024-01-05 23:49:56 +0000

    for x num of .make calls

---
+++ plume/main_dj.py
@@ -0,0 +1,16 @@
+from plume.schema import *
+from multiprocessing import Process, Pipe
+import sys
+
+if __name__ == '__main__':
+    num_of_processes = sys.argv[1]
+    processes = []
+    kw={'reserve_jobs':True}
+    for i in num_of_processes:
+        processes.append(target=TrainingResult.populate, kwargs=kw)
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
