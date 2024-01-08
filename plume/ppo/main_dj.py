5ee37087c28436ecae316b22fd17b75be6ac8b1b
lolrenceH <lawrenhu@bu.edu>
2024-01-08 23:50:50 +0000

    debug main_dj. still not workiing

--- plume/ppo/main_dj.py
+++ plume/ppo/main_dj.py
@@ -3,10 +3,10 @@ from multiprocessing import Process, Pipe
 import sys
 
 if __name__ == '__main__':
-    num_of_processes = sys.argv[1]
+    num_of_processes = int(sys.argv[1])
     processes = []
     kw={'reserve_jobs':True}
-    for i in num_of_processes:
+    for i in range(num_of_processes):
         processes.append(Process(target=TrainingResult.populate, kwargs=kw))
 
     for p in processes:
