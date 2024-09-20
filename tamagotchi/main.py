e3496958362d14b7ccabda09ae7187cbd219a2f0
lolrenceH <lawrenhu@bu.edu>
2024-09-20 21:04:22 +0000

    main, training - if vec norm update

--- tamagotchi/main.py
+++ tamagotchi/main.py
@@ -127,8 +127,8 @@ def get_args():
     parser.add_argument('--model_fname',  type=str, default='')
     parser.add_argument('--obs_noise', type=float, default=0.0)
     parser.add_argument('--act_noise', type=float, default=0.0)
-    parser.add_argument('--if_vec_norm', type=float, default=0.0) # whether to normalize the input
-    parser.add_argument('--if_train_actor_std', type=float, default=0.0) # whether to train the std of the stochastic policy
+    parser.add_argument('--if_vec_norm', type=bool, default=True) # whether to normalize the input
+    parser.add_argument('--if_train_actor_std', type=bool, default=False) # whether to train the std of the stochastic policy
     args = parser.parse_args()
     assert len(args.dataset) == len(args.qvar) 
     assert len(args.dataset) == len(args.diff_max) 
