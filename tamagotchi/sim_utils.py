edba12a921baf70e0a043e47e096497c8de16fd8
lolrenceH <lawrenhu@bu.edu>
2025-02-19 00:47:25 +0000

    dataset name typo in sim utils

--- tamagotchi/sim_utils.py
+++ tamagotchi/sim_utils.py
@@ -89,7 +89,7 @@ def get_wind_vectors_flexible(T, wind_magnitude, local_state=None, regime=None):
         wind_degrees += noise # no smoothing
 
     # Add random noise to wind degree
-    if 'poisson_noisy' in regime:
+    if 'poisson_noisy3' in regime:
         # based on /src/JH_boilerplate/check_noisy_wind_dirs.ipynb
         noise = np.zeros(len(T)) # Init
         degz = 60 # +/- degz 
